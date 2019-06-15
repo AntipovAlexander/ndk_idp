ba8289071a345b87060547a83fe4f6bf158fcb2f
Alexander Antipov <antipovaleksandr95@gmail.com>
2019-06-15 20:02:29 +0300

    refactored captcha activity

--- app/src/main/java/com/antipov/ndk_idp/CaptchaActivity.kt
+++ app/src/main/java/com/antipov/ndk_idp/CaptchaActivity.kt
@@ -1,32 +1,64 @@
 package com.antipov.ndk_idp
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import androidx.core.view.isVisible
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_captcha.*
+import org.jetbrains.anko.toast
 import java.io.File
 
 class CaptchaActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_captcha)
-
-        // create a file
-        val file = File(filesDir, "captcha.bmp")
-
-        makeCaptcha(true, file.absolutePath)
-
-        val myBitmap = BitmapFactory.decodeFile(file.absolutePath)
-
-        captcha.setImageBitmap(myBitmap)
-    }
-
-    private external fun makeCaptcha(addBorder: Boolean, path: String): Boolean
-
     companion object {
+        private const val FILENAME = "captcha.bmp"
+
         init {
             System.loadLibrary("cimg-test")
         }
     }
+
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_captcha)
+        refresh.setOnRefreshListener { generateCaptcha() }
+        generateCaptcha()
+    }
+
+    private fun generateCaptcha() {
+        Single.fromCallable {
+            // create a file
+            val file = File(filesDir, FILENAME)
+            // call native function
+            makeCaptcha(true, file.absolutePath)
+            // decoding generated bitmap with captcha
+            BitmapFactory.decodeFile(file.absolutePath)
+        }
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(::onCaptchaGenerated, ::onCaptchaError)
+            .addToDisposables()
+    }
+
+    private fun onCaptchaError(throwable: Throwable) {
+        toast(throwable.toString())
+    }
+
+    private fun onCaptchaGenerated(bmp: Bitmap) {
+        captcha.setImageBitmap(bmp)
+        refresh.isRefreshing = false
+        if (progress.isVisible) progress.isVisible = false
+    }
+
+    private external fun makeCaptcha(addBorder: Boolean, path: String): Boolean
+
+    private fun Disposable.addToDisposables() = compositeDisposable.add(this)
 }
