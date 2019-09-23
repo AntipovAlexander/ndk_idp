4d5b1bd259307dd121aaed4472e77197156e977c
Alexander Antipov <antipov@themindstudios.com>
2019-09-23 12:52:04 +0300

    added logger

--- app/src/main/cpp/quadratic/quadratic-equation.cpp
+++ app/src/main/cpp/quadratic/quadratic-equation.cpp
@@ -1,6 +1,14 @@
 #include <jni.h>
 #include <string>
 #include <cmath>
+#include <android/log.h>
+
+#define TAG "CPP_QUADRATIC_SOLVER"
+
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,    TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,     TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,     TAG, __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,    TAG, __VA_ARGS__)
 
 using namespace std;
 extern "C" JNIEXPORT jdoubleArray JNICALL
@@ -11,6 +19,12 @@ Java_com_antipov_ndk_1idp_MainActivity_quadraticEquation(
         jint b,
         jint c) {
 
+    LOGI("Received params");
+    LOGI("A = %d", a);
+    LOGI("B = %d", b);
+    LOGI("C = %d", c);
+    LOGI("Computing ...");
+
     if ((b * b - 4 * a * c) >= 0) {
         // calculating firs and second roots
         double first = (-1 * b + sqrt(b * b - 4 * a * c)) / (2 * a);
@@ -20,10 +34,13 @@ Java_com_antipov_ndk_1idp_MainActivity_quadraticEquation(
         jdoubleArray javaArray = env->NewDoubleArray(2);
         env->SetDoubleArrayRegion(javaArray, 0, 2, values);
         // return roots
+        LOGI("First root is = %f", values[0]);
+        LOGI("Second root is = %f", values[1]);
         return javaArray;
     } else {
         // id D < 0 throw exception to java
         jclass exception = env->FindClass("java/lang/Exception");
+        LOGE("D < 0, abort.");
         return reinterpret_cast<jdoubleArray>(env->ThrowNew(exception, "D < 0, abort."));
     }
 }
